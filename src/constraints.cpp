d7592fa5c24e621e1caf74a6d66ee1df035e6687
Léon Othenin-Girard <lgirard@ethz.ch>
2025-11-20 18:27:58 +0100

    fixing size issues in constraint calculations, but now the calculation is still wrong unfortunately

--- src/constraints.cpp
+++ src/constraints.cpp
@@ -47,7 +47,7 @@ bool DiscreteElasticRod::getConstraints(const Optimization::VectorXf &x_lambda, 
     Eigen::VectorXf lambda = x_lambda.tail(m_n + 1);
 
     //this is assuming that the output value is a scalar (using y^TMy instead of the yMy^T written in the paper), because otherwise the dimensions of this calculation don't work
-    energy = 0.5f * (x_lambda.head(3 * (m_n + 2)) - m_vertex_positions).squaredNorm() - C.dot(lambda); // TODO: Cannot take squared norm on matrix
+    energy = 0.5f * (x_lambda.head(3 * (m_n + 2)) - m_vertex_positions.reshaped(3 * (m_n + 2), 1)).squaredNorm() - C.dot(lambda);
 
     return true;
 }
@@ -61,14 +61,14 @@ bool DiscreteElasticRod::getConstraintGradient(const Optimization::VectorXf &x_l
 
     // contribution from loss function
     Eigen::VectorXf lambdas = x_lambda.tail(m_n + 1);
-    X_constraints = x_lambda.head(3 * (m_n + 2)) - m_vertex_positions;
+    X_constraints = x_lambda.head(3 * (m_n + 2)) - m_vertex_positions.reshaped(3 * (m_n + 2), 1);
 
     // contribution from inextensibility constraints
     for (int i = 0; i <= m_n; i++) {
-        X_constraints.segment<3>(3 * i) += 2 * lambdas(i) * (x_lambda.segment<3>(3 * i) - x_lambda.segment<3>(3 * (i + 1)));
+        X_constraints.segment<3>(3 * i) -= 2 * lambdas(i) * (x_lambda.segment<3>(3 * i) - x_lambda.segment<3>(3 * (i + 1)));
     }
     for (int i = 1; i <= m_n + 1; i++) {
-        X_constraints.segment<3>(3 * i) += 2 * lambdas(i-1) * (x_lambda.segment<3>(3 * i) - x_lambda.segment<3>(3 * (i - 1)));
+        X_constraints.segment<3>(3 * i) -= 2 * lambdas(i-1) * (x_lambda.segment<3>(3 * i) - x_lambda.segment<3>(3 * (i - 1)));
     }
 
     Eigen::VectorXf C = Eigen::VectorXf::Zero(m_n + 1);
@@ -78,7 +78,7 @@ bool DiscreteElasticRod::getConstraintGradient(const Optimization::VectorXf &x_l
     gradient = Eigen::VectorXf::Zero(3 * (m_n + 2) + m_n + 1);
 
     gradient.head(3 * (m_n + 2)) = X_constraints;
-    gradient.tail(m_n + 1) = C;
+    gradient.tail(m_n + 1) = -C;
 
     return true;
 }
@@ -94,37 +94,42 @@ bool DiscreteElasticRod::getConstraintHessian(const Optimization::VectorXf &x_la
     for (int i = 0; i <= m_n+1; i++) {
         for (int j = 0; j < 3; j++) {
             if (i == 0) {
-                hessian.emplace_back(0 + j, 0 + j, 1.f + 2.f * lambda(0));
-                hessian.emplace_back(0 + j, 3 + j, -2.f * lambda(0));
+                hessian.emplace_back(0 + j, 0 + j, 1.f - 2.f * lambda(0));
+                hessian.emplace_back(0 + j, 3 + j, 2.f * lambda(0));
 
             } else if (i == m_n + 1) {
-                hessian.emplace_back(3 * (m_n + 1) + j, 3 * (m_n + 1) + j, 1.f + 2.f * lambda(m_n));
-                hessian.emplace_back(3 * (m_n + 1) + j, 3 * m_n + j, -2.f * lambda(m_n));
+                hessian.emplace_back(3 * (m_n + 1) + j, 3 * (m_n + 1) + j, 1.f - 2.f * lambda(m_n));
+                hessian.emplace_back(3 * (m_n + 1) + j, 3 * m_n + j, 2.f * lambda(m_n));
 
             } else {
-                hessian.emplace_back(3 * i + j, 3 * i + j, 1.f + 2.f * lambda(i) + 2.f * lambda(i-1));
-                hessian.emplace_back(3 * i + j, 3 * (i+1) + j, - 2.f * lambda(i));
-                hessian.emplace_back(3 * i + j, 3 * (i-1) + j, - 2.f * lambda(i-1));
+                hessian.emplace_back(3 * i + j, 3 * i + j, 1.f - 2.f * lambda(i) - 2.f * lambda(i-1));
+                hessian.emplace_back(3 * i + j, 3 * (i+1) + j,  2.f * lambda(i));
+                hessian.emplace_back(3 * i + j, 3 * (i-1) + j,  2.f * lambda(i-1));
             }
         }
     }
 
     for (int i = 0; i <= m_n; i++) {
         for (int j = 0; j < 3; j++) {
-            int dl_index = 3 * (m_n + 2 + i) + j;
+            int dl_index = 3 * (m_n + 2) + i;
             int dx_index = 3 * i + j;
 
-            float x_i_minus_x_i_plus_one = 2 * (x(3 * i + j) - x(3 * (i + 1) + j));
-            float x_i_plus_one_minus_x_i = 2 * (x(3 * (i + 1) + j) - x(3 * i + j));
+            float x_i_minus_x_i_plus_one = 2 * (x(dx_index) - x(dx_index + 3));
+            float x_i_plus_one_minus_x_i = 2 * (x(dx_index + 3) - x(dx_index));
 
             hessian.emplace_back(dl_index, dx_index, x_i_minus_x_i_plus_one);
             hessian.emplace_back(dl_index, dx_index + 3, x_i_plus_one_minus_x_i);
 
             hessian.emplace_back(dx_index, dl_index, x_i_minus_x_i_plus_one);
             hessian.emplace_back(dx_index + 3, dl_index, x_i_plus_one_minus_x_i);
+
+            // print_debug(x_i_minus_x_i_plus_one);
+            // print_debug(x_i_plus_one_minus_x_i);
         }
 
     }
+
+
 
     return true;
 }
