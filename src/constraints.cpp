00348c592c1fee555f747c45f659705d46373041
Dillon Elste <djantivenom@yahoo.com>
2025-11-24 19:17:00 +0100

    Fix compilation error bc of change to double data-type

--- src/constraints.cpp
+++ src/constraints.cpp
@@ -4,12 +4,12 @@
 
 #include <stdexcept>
 
-bool DiscreteElasticRod::getConstraints(const Optimization::VectorXf &x_lambda, double &energy) const {
-    print_debug("DiscreteElasticRod::getConstraints");
+bool DiscreteElasticRod::getConstraints(const Opt::VectorX &x_lambda, Opt::Float &energy) const {
+    print_debug("");
 
     Eigen::VectorXf C = Eigen::VectorXf::Zero(m_n + 1);
     for (int i = 0; i <= m_n; i++) {
-        float new_edge_length = (x_lambda.segment<3>(3 * (i+1)) - x_lambda.segment<3>(3 * i)).norm();
+        Float new_edge_length = (x_lambda.segment<3>(3 * (i+1)) - x_lambda.segment<3>(3 * i)).norm();
         // $E_s = 1/2 (|x_{i+1} - x_i| - |\bar{e_i}|)^2$
         C(i) = 0.5f * std::pow(new_edge_length - m_edge_length(i), 2);
     }
@@ -19,24 +19,24 @@ bool DiscreteElasticRod::getConstraints(const Optimization::VectorXf &x_lambda, 
     return true;
 }
 
-bool DiscreteElasticRod::getConstraintGradient(const Optimization::VectorXf &x_lambda, Optimization::VectorXf &gradient) const {
-    print_debug("DiscreteElasticRod::getConstraintGradient");
+bool DiscreteElasticRod::getConstraintGradient(const Opt::VectorX &x_lambda, Opt::VectorX &gradient) const {
+    print_debug("");
 
-    auto maple_gradient = [](const Eigen::Vector3f &x_min_1, const Eigen::Vector3f &x_i, const Eigen::Vector3f &x_plus_1, const Eigen::Vector2f &e_bar)
+    auto maple_gradient = [](const Vector3 &x_min_1, const Vector3 &x_i, const Vector3 &x_plus_1, const Vector2 &e_bar)
     {
-        Eigen::Vector3f dC_Stretch;
+        Vector3 dC_Stretch;
         dC_Stretch.setZero();
 
-        float t1 = x_min_1[0] - x_i[0];
-        float t2 = x_min_1[1] - x_i[1];
-        float t3 = x_min_1[2] - x_i[2];
-        float t4 = pow(t1, 0.2e1) + pow(t2, 0.2e1) + pow(t3, 0.2e1);
-        float t5 = pow(t4, -0.1e1 / 0.2e1);
-        float t6 = -x_plus_1[0] + x_i[0];
-        float t7 = -x_plus_1[1] + x_i[1];
-        float t8 = -x_plus_1[2] + x_i[2];
-        float t9 = pow(t6, 0.2e1) + pow(t7, 0.2e1) + pow(t8, 0.2e1);
-        float t10 = pow(t9, -0.1e1 / 0.2e1);
+        Float t1 = x_min_1[0] - x_i[0];
+        Float t2 = x_min_1[1] - x_i[1];
+        Float t3 = x_min_1[2] - x_i[2];
+        Float t4 = pow(t1, 0.2e1) + pow(t2, 0.2e1) + pow(t3, 0.2e1);
+        Float t5 = pow(t4, -0.1e1 / 0.2e1);
+        Float t6 = -x_plus_1[0] + x_i[0];
+        Float t7 = -x_plus_1[1] + x_i[1];
+        Float t8 = -x_plus_1[2] + x_i[2];
+        Float t9 = pow(t6, 0.2e1) + pow(t7, 0.2e1) + pow(t8, 0.2e1);
+        Float t10 = pow(t9, -0.1e1 / 0.2e1);
         t9 = (t9 * t10 - e_bar[1]) * t10;
         t4 = (t4 * t5 - e_bar[0]) * t5;
         dC_Stretch[0] = -t4 * t1 + t9 * t6;
@@ -46,7 +46,7 @@ bool DiscreteElasticRod::getConstraintGradient(const Optimization::VectorXf &x_l
         return dC_Stretch;
     };
 
-    gradient = Eigen::VectorXf::Zero(3 * (m_n + 2));
+    gradient = VectorX::Zero(3 * (m_n + 2));
 
     // For inextensibility, do not allow vertices 0 and n+1 to move, since they are fixed.
     // TODO: Validate if this sum range is correct
@@ -55,7 +55,7 @@ bool DiscreteElasticRod::getConstraintGradient(const Optimization::VectorXf &x_l
             x_lambda.segment<3>(3 * (i-1)),
             x_lambda.segment<3>(3 * i),
             x_lambda.segment<3>(3 * (i+1)),
-            Eigen::Vector2f(m_edge_length(i-1), m_edge_length(i)));
+            Vector2(m_edge_length(i-1), m_edge_length(i)));
     }
 
     print_debug("constraint gradient magnitude is " + std::to_string(gradient.norm()));
@@ -63,40 +63,40 @@ bool DiscreteElasticRod::getConstraintGradient(const Optimization::VectorXf &x_l
 }
 
 
-bool DiscreteElasticRod::getConstraintHessian(const Optimization::VectorXf &x_lambda, Optimization::TripletListF &hessian) const {
+bool DiscreteElasticRod::getConstraintHessian(const Opt::VectorX &x_lambda, Opt::TripletList &hessian) const {
     print_debug("DiscreteElasticRod::getConstraintHessian");
 
-    auto maple_hessian = [](const Eigen::Vector3f &x_min_1, const Eigen::Vector3f &x_i, const Eigen::Vector3f &x_plus_1, const Eigen::Vector2f &e_bar)
+    auto maple_hessian = [](const Vector3 &x_min_1, const Vector3 &x_i, const Vector3 &x_plus_1, const Vector2 &e_bar)
     {
-        Eigen::Matrix<float, 9, 1> d2C_Stretch;
+        Eigen::Matrix<Float, 9, 1> d2C_Stretch;
         d2C_Stretch.setZero();
 
-        float t1 = x_min_1[0] - x_i[0];
-        float t2 = x_min_1[1] - x_i[1];
-        float t3 = x_min_1[2] - x_i[2];
-        float t4 = pow(t1, 0.2e1);
-        float t5 = pow(t2, 0.2e1);
-        float t6 = pow(t3, 0.2e1);
-        float t7 = t6 + t5 + t4;
-        float t8 = pow(t7, -0.3e1 / 0.2e1);
-        float t9 = -pow(t7, 0.2e1) * t8 + e_bar[0];
-        float t10 = t7 * t8;
-        float t11 = x_i[0] - x_plus_1[0];
-        float t12 = x_i[1] - x_plus_1[1];
-        float t13 = x_i[2] - x_plus_1[2];
-        float t14 = pow(t11, 0.2e1);
-        float t15 = pow(t12, 0.2e1);
-        float t16 = pow(t13, 0.2e1);
-        float t17 = t16 + t15 + t14;
-        float t18 = pow(t17, -0.3e1 / 0.2e1);
-        float t19 = -pow(t17, 0.2e1) * t18 + e_bar[1];
-        float t20 = t17 * t18;
+        Float t1 = x_min_1[0] - x_i[0];
+        Float t2 = x_min_1[1] - x_i[1];
+        Float t3 = x_min_1[2] - x_i[2];
+        Float t4 = pow(t1, 0.2e1);
+        Float t5 = pow(t2, 0.2e1);
+        Float t6 = pow(t3, 0.2e1);
+        Float t7 = t6 + t5 + t4;
+        Float t8 = pow(t7, -0.3e1 / 0.2e1);
+        Float t9 = -pow(t7, 0.2e1) * t8 + e_bar[0];
+        Float t10 = t7 * t8;
+        Float t11 = x_i[0] - x_plus_1[0];
+        Float t12 = x_i[1] - x_plus_1[1];
+        Float t13 = x_i[2] - x_plus_1[2];
+        Float t14 = pow(t11, 0.2e1);
+        Float t15 = pow(t12, 0.2e1);
+        Float t16 = pow(t13, 0.2e1);
+        Float t17 = t16 + t15 + t14;
+        Float t18 = pow(t17, -0.3e1 / 0.2e1);
+        Float t19 = -pow(t17, 0.2e1) * t18 + e_bar[1];
+        Float t20 = t17 * t18;
         t7 = 0.1e1 / t7;
         t17 = 0.1e1 / t17;
-        float t21 = t18 * t19 + t17;
-        float t22 = t8 * t9 + t7;
+        Float t21 = t18 * t19 + t17;
+        Float t22 = t8 * t9 + t7;
         t2 = t22 * t2;
-        float t23 = t12 * t11 * t21 + t2 * t1;
+        Float t23 = t12 * t11 * t21 + t2 * t1;
         t1 = t22 * t3 * t1 + t13 * t11 * t21;
         t2 = t13 * t12 * t21 + t2 * t3;
         d2C_Stretch[0] = t14 * t17 + t19 * (t14 * t18 - t20) + t4 * t7 + t9 * (t4 * t8 - t10);
@@ -114,12 +114,12 @@ bool DiscreteElasticRod::getConstraintHessian(const Optimization::VectorXf &x_la
 
     for (int i = 1; i <= m_n; i++)
     {
-        Eigen::Vector3f x_min_1 = x_lambda.segment<3>(3 * (i-1));
-        Eigen::Vector3f x_i = x_lambda.segment<3>(3 * i);
-        Eigen::Vector3f x_plus_1 = x_lambda.segment<3>(3 * (i+1));
-        Eigen::Vector2f e_bar = Eigen::Vector2f(m_edge_length(i-1), m_edge_length(i));
+        Vector3 x_min_1 = x_lambda.segment<3>(3 * (i-1));
+        Vector3 x_i = x_lambda.segment<3>(3 * i);
+        Vector3 x_plus_1 = x_lambda.segment<3>(3 * (i+1));
+        Vector2 e_bar = Vector2(m_edge_length(i-1), m_edge_length(i));
 
-        Eigen::Matrix3f xi_hessian = maple_hessian(
+        Matrix3 xi_hessian = maple_hessian(
             x_min_1,
             x_i,
             x_plus_1,
@@ -143,24 +143,24 @@ bool DiscreteElasticRod::getConstraintHessian(const Optimization::VectorXf &x_la
 void DiscreteElasticRod::applyConstraints(size_t max_newton_iterations) {
     print_debug("[applyConstraints] enter");
 
-    Optimization opt;
-    opt.optimizer = Optimization::Optimizer::NEWTON;
+    Opt opt;
+    opt.optimizer = Opt::Optimizer::NEWTON;
     opt.tolerance_exponent = -10;
-    opt.objective_function = [&](const Eigen::VectorXf &x_lambda, double &energy)
+    opt.objective_function = [&](const Opt::VectorX &x_lambda, Opt::Float &energy)
         {
             return getConstraints(x_lambda, energy);
         };
-    opt.gradient_function = [&](const Eigen::VectorXf &x_lambda,
-                                double &energy,
-                                Eigen::VectorXf &gradient)
+    opt.gradient_function = [&](const Opt::VectorX &x_lambda,
+                                Opt::Float &energy,
+                                Opt::VectorX &gradient)
         {
             getConstraints(x_lambda, energy);
             return getConstraintGradient(x_lambda, gradient);
         };
-    opt.hessian_function = [&](const Optimization::VectorXf &x_lambda,
-                                double &energy,
-                                Optimization::VectorXf &gradient,
-                                Optimization::TripletListF &hessian)
+    opt.hessian_function = [&](const Opt::VectorX &x_lambda,
+                                Opt::Float &energy,
+                                Opt::VectorX &gradient,
+                                Opt::TripletList &hessian)
         {
             getConstraints(x_lambda, energy);
             getConstraintGradient(x_lambda, gradient);
@@ -173,19 +173,19 @@ void DiscreteElasticRod::applyConstraints(size_t max_newton_iterations) {
     print_debug(getEdges().colwise().norm());
 
     for (size_t step = 0; step < max_newton_iterations; ++step) {
-        Eigen::VectorXf x_lambda = Eigen::VectorXf::Zero(3 * (m_n + 2));
+        VectorX x_lambda = VectorX::Zero(3 * (m_n + 2));
         x_lambda.head(3 * (m_n + 2)) = m_vertex_positions.reshaped(3 * (m_n + 2), 1);
 
         auto result = opt.step(x_lambda);
         switch (result)
         {
-            case Optimization::OptimizationStatus::SUCCESS:
+            case Opt::OptimizationStatus::SUCCESS:
                 m_vertex_positions = x_lambda.head(3 * (m_n + 2)).reshaped(3, m_n + 2);
                 break;
-            case Optimization::OptimizationStatus::FAILURE:
+            case Opt::OptimizationStatus::FAILURE:
                 std::cout << "constraint calculation fail" << std::endl;
                 return;
-            case Optimization::OptimizationStatus::CONVERGED:
+            case Opt::OptimizationStatus::CONVERGED:
 
                 print_debug("Resulting positions:");
                 print_debug(m_vertex_positions);
