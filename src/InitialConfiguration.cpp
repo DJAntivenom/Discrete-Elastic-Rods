8e427f22030f7c8e1e26dbf82568b5c8013fe090
Dillon Elste <delste@student.ethz.ch>
2025-12-12 16:54:49 +0100

    Implement circle segment

--- src/InitialConfiguration.cpp
+++ src/InitialConfiguration.cpp
@@ -26,69 +26,20 @@ public:
         InitialConfiguration(n, radius)
     {
         /**
-         * We want to compress the rod to 0 < q < 1 of its original length.
-         * For simplicity the rod will be bent s.t. there is a single kink.
-         * Its new shape combined with the original straight rod compressed
-         * to q*length will form an issceles triangle. With pythagoras we
-         * can calculate h
-         *
-         *    q*length
-         * ---------------
-         * \            /
-         *  \          /
-         *   \        /
-         * l/2\      / l/2
-         *     \    /
-         *      \  /
-         *       \/
-         *
-         * => h = length * sqrt(1/4 - q²/4)
-         *
-         * Some care has to be taken if n is even
-         *    q*length
-         * ---------------
-         * \            /
-         *  \          /
-         *   \        /
-         *    \      / nl/(2(n+1))
-         *     \    /
-         *      \__/
-         *      l/(n+1)
+         * We want to have the rod form a circle-segment defined by angle q using
+         * equally spaced angle segments.
          */
         m_rest_omega.setZero();
-        const Float q = 0.9;
-        m_initial_positions.col(0) = Vector3{ -q * length * 0.5, 0, 0 };
-        m_initial_positions.col(n + 1) = Vector3{ q * length * 0.5, 0, 0 };
-        const Float l = length / (n + 1);
+        /* 30° */
+        constexpr double q = M_PI / 6;
+        const double h = length / (2 * std::tan(q * 0.5));
+        const double r = std::sqrt(h * h + length * length * 0.25);
+        const Vector3 center{ 0, h, 0 };
 
-        Vector3 x0_to_kink, xnp1_to_kink;
-        if (n % 2 == 1)
+        for (uint64_t i = 0; i <= n + 1; ++i)
         {
-            const Float h = length * std::sqrt(0.25 - q * q * 0.25);
-            const Vector3 kink{ 0,-h,0 };
-
-            x0_to_kink = (kink - m_initial_positions.col(0)).normalized();
-            xnp1_to_kink = (kink - m_initial_positions.col(n + 1)).normalized();
-            m_initial_positions.col((n + 2) / 2) = kink;
-        }
-        else
-        {
-            const Float h = std::sqrt(std::pow(n * length / (2 * (n + 1)), 2) - std::pow((length * q - length / (n + 1)) / 2, 2));
-
-            x0_to_kink = (Vector3{ -length * 0.5 / (n + 1), -h, 0 } - m_initial_positions.col(0)).normalized();
-            xnp1_to_kink = (Vector3{ length * 0.5 / (n + 1), -h, 0 } - m_initial_positions.col(n + 1)).normalized();
-        }
-
-        /**
-         * If n is odd we have a vertex at the kink and there are
-         * (n+2) / 2 vertices per leg but we can skip the fixed positions.
-         * If n is even there is no vertex at the kink but there are still
-         * (n+2) / 2 vertices per leg and we can skip the fixed positions
-         */
-        for (uint64_t i = 1; i < (n + 2) / 2; ++i)
-        {
-            m_initial_positions.col(i) = m_initial_positions.col(0) + x0_to_kink * l * i;
-            m_initial_positions.col(n + 1 - i) = m_initial_positions.col(n + 1) + xnp1_to_kink * l * i;
+            const Float angle = q * i / (n + 1) + (M_PI + M_PI_2 - q / 2);
+            m_initial_positions.col(i) = r * Vector3{ std::cos(angle), std::sin(angle), 0 } + center;
         }
     }
 };
